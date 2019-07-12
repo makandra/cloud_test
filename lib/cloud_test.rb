d724b6c1c63d134e6397f849b1672f7badb62def
Philipp Häusele <philipp.haeusele@makandra.de>
2019-07-12 16:04:28 +0200

    minor changes

--- lib/cloud_test.rb
+++ lib/cloud_test.rb
@@ -12,11 +12,11 @@ module CloudTest
     CloudTest::Core.get_provider_class(config).init config
   end
 
-  at_exit do
-    if $!.nil? || $!.is_a?(SystemExit) && $!.success?
+  at_exit do # is this better in here or in the cloud_test.rb file with the Before tag?
+    if $!.nil? || $!.is_a?(SystemExit) && $!.success? # if test was successful
       Core.list_dashboard_link
     else
-      code = $!.is_a?(SystemExit) ? $!.status : 1
+      code = $!.is_a?(SystemExit) ? $!.status : 1 #if test was not successful, keep exit code
       Core.list_dashboard_link
       exit code
     end
